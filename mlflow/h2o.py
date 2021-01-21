9beadd537d18b78ef0f068955b3a2aa1dcbbf92c
vedantja <vedjain85@gmail.com>
2021-01-21 16:53:00 -0600

    added download_mojo function to mlflow.h2o

--- mlflow/h2o.py
+++ mlflow/h2o.py
@@ -285,3 +285,109 @@ def load_model(model_uri):
     # `data` key; in this case, we assume the model artifact path to be `model.h2o`
     h2o_model_file_path = os.path.join(local_model_path, flavor_conf.get("data", "model.h2o"))
     return _load_model(path=h2o_model_file_path)
+
+
+def save_mojo(
+    h2o_model,
+    path,
+    conda_env=None,
+    mlflow_model=None,
+    settings=None,
+    signature: ModelSignature = None,
+    input_example: ModelInputExample = None,
+):
+    """
+    Save an H2O model to a path on the local file system.
+
+    :param h2o_model: H2O model to be saved.
+    :param path: Local path where the model is to be saved.
+    :param conda_env: Either a dictionary representation of a Conda environment or the path to a
+                      Conda environment yaml file. If provided, this describes the environment
+                      this model should be run in. At minimum, it should specify the dependencies
+                      contained in :func:`get_default_conda_env()`. If ``None``, the default
+                      :func:`get_default_conda_env()` environment is added to the model.
+                      The following is an *example* dictionary representation of a Conda
+                      environment::
+
+                        {
+                            'name': 'mlflow-env',
+                            'channels': ['defaults'],
+                            'dependencies': [
+                                'python=3.7.0',
+                                'pip': [
+                                    'h2o==3.20.0.8'
+                                ]
+                            ]
+                        }
+
+    :param signature: (Experimental) :py:class:`ModelSignature <mlflow.models.ModelSignature>`
+                      describes model input and output :py:class:`Schema <mlflow.types.Schema>`.
+                      The model signature can be :py:func:`inferred <mlflow.models.infer_signature>`
+                      from datasets with valid model input (e.g. the training dataset with target
+                      column omitted) and valid model output (e.g. model predictions generated on
+                      the training dataset), for example:
+
+                      .. code-block:: python
+
+                        from mlflow.models.signature import infer_signature
+                        train = df.drop_column("target_label")
+                        predictions = ... # compute model predictions
+                        signature = infer_signature(train, predictions)
+    :param input_example: (Experimental) Input example provides one or several instances of valid
+                          model input. The example can be used as a hint of what data to feed the
+                          model. The given example will be converted to a Pandas DataFrame and then
+                          serialized to json using the Pandas split-oriented format. Bytes are
+                          base64-encoded.
+
+    :param mlflow_model: :py:mod:`mlflow.models.Model` this flavor is being added to.
+    """
+    import h2o
+
+    path = os.path.abspath(path)
+    if os.path.exists(path):
+        raise Exception("Path '{}' already exists".format(path))
+    model_data_subpath = "model.h2o"
+    model_data_path = os.path.join(path, model_data_subpath)
+    os.makedirs(model_data_path)
+
+    if mlflow_model is None:
+        mlflow_model = Model()
+    if signature is not None:
+        mlflow_model.signature = signature
+    if input_example is not None:
+        _save_example(mlflow_model, input_example, path)
+
+    # Save h2o-model
+    if hasattr(h2o, "download_mojo"):
+        h2o_save_location = h2o.download_mojo(model=h2o_model, path=model_data_path, get_genmodel_jar=True)
+    else:
+        warnings.warn(
+            "If your cluster is remote, H2O may not store the model correctly. "
+            "Please upgrade H2O version to a newer version"
+        )
+        h2o_save_location = h2o.save_mojo(model=h2o_model, path=model_data_path, force=True)
+    model_file = os.path.basename(h2o_save_location)
+
+    # Save h2o-settings
+    if settings is None:
+        settings = {}
+    settings["full_file"] = h2o_save_location
+    settings["model_file"] = model_file
+    settings["model_dir"] = model_data_path
+    with open(os.path.join(model_data_path, "h2o.yaml"), "w") as settings_file:
+        yaml.safe_dump(settings, stream=settings_file)
+
+    conda_env_subpath = "conda.yaml"
+    if conda_env is None:
+        conda_env = get_default_conda_env()
+    elif not isinstance(conda_env, dict):
+        with open(conda_env, "r") as f:
+            conda_env = yaml.safe_load(f)
+    with open(os.path.join(path, conda_env_subpath), "w") as f:
+        yaml.safe_dump(conda_env, stream=f, default_flow_style=False)
+
+    pyfunc.add_to_model(
+        mlflow_model, loader_module="mlflow.h2o", data=model_data_subpath, env=conda_env_subpath
+    )
+    mlflow_model.add_flavor(FLAVOR_NAME, h2o_version=h2o.__version__, data=model_data_subpath)
+    mlflow_model.save(os.path.join(path, MLMODEL_FILE_NAME))
